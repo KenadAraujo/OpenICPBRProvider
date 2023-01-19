06c8e5395aef5a351376431b57c81795fd328ae8
kenad <kenad@sefaz.pi.gov.br>
2023-01-19 13:26:46 -0300

    -Projeto inicial

---
+++ src/main/java/org/polligonalApps/OpenICPBRProvider/OpenIcpbrProviderApplication.java
@@ -0,0 +1,13 @@
+package org.polligonalApps.OpenICPBRProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenIcpbrProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenIcpbrProviderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/polligonalApps/OpenICPBRProvider/OpenIcpbrProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.polligonalApps.OpenICPBRProvider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenIcpbrProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
